182aa42c847cd3f95912839e0f6e0fd810ae583e
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-03-21 12:53:59 +0700

    fix(script): build didn't include tailwindcss correctly
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,13 +1,21 @@
 import { join } from "node:path";
+import tailwind from "bun-plugin-tailwind";
 
-const cwd = process.cwd();
-const output = await Bun.build({
-	entrypoints: [join(cwd, "public/index.html")],
-	target: "browser",
-	outdir: join(cwd, "dist"),
-	minify: true,
-	sourcemap: "linked",
-});
+async function build() {
+	const cwd = process.cwd();
+	const plugins = [tailwind];
+
+	return Bun.build({
+		entrypoints: [join(cwd, "public/index.html")],
+		target: "browser",
+		outdir: join(cwd, "dist"),
+		minify: true,
+		sourcemap: "linked",
+		plugins,
+	});
+}
+
+const output = await build();
 
 console.log(
 	output.logs.map((l) => `${l.level}: ${l.name} - ${l.message}`).join("\n"),
